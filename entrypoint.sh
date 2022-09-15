06e3bc91d0b397b2ce356eea6b9c2ce688186284
gourab103111 <gourab.singha@yahoo.in>
2022-09-15 18:27:11 +0530

    Start commit 15-09-22 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 #hub release create -a ./test1-release.apk -m "v${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
 hub checkout ${${REPO_BRANCH}:-master}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+hub release create -a ./test-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
